04bee331365672b463c4df6776622bd3bb8cf0a5
Julian <juliandisterheft@gmail.com>
2019-02-22 22:34:36 -0500

    no message

---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour {
+
+    public GameObject[] objectsToSpawn;
+
+    public GameObject tileObject;
+    public enum TileToSpawn { Empty, Tiled };
+
+
+    private void Start()
+    {
+        //TileToSpawn tile;
+
+         
+       // tile = TileToSpawn.Empty;
+        int rand = Random.Range(0, objectsToSpawn.Length);
+        GameObject instance = Instantiate(objectsToSpawn[rand], transform.position, Quaternion.identity);
+        instance.transform.parent = transform;
+
+       
+    }
+}
